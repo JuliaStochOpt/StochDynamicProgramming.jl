84fd6434a0efea18e6c2522c672e12dc5abb9348
fpacaud <f.pacaud@efficacity.com>
2016-02-01 18:12:23 +0100

    [UPD] Add a function to get analytic cost of LQR

--- examples/lqr.jl
+++ examples/lqr.jl
@@ -1,6 +1,15 @@
+#  Copyright 2015, Vincent Leclere, Francois Pacaud and Henri Gerard
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#############################################################################
 # Implement a linear quadratic feedback controller.
-
 # Source: Bertsekas, Dynamic Programming and Optimal Control
+#############################################################################
+
+
+
+
 
 using PyPlot
 
@@ -30,18 +39,25 @@ Returns:
     - `L`: evolution of gains, indexed by time.
 
 """
-function solve_riccati_backward(Tf, A, B, Q, R, Sf)
+function solve_riccati_backward(Tf::Int64,
+                                A, #::Array{Float64, 2},
+                                B, #::Array{Float64, 2},
+                                Q, #::Array{Float64, 2},
+                                R, #::Array{Float64, 2},
+                                Sf,) #::Array{Float64, 2})
     # This array will store evolution of Riccati gain;=:
     L = zeros(Tf, 1, 2)
+    P_array = zeros(Tf, 2, 2)
     P = Sf
 
     for i=(Tf-1):-1:1
         K = -inv(R + B'*P*B)*B'*P*A
         P = Q + A'*P*A + A'*P*B*K
         L[i, :, :] = K
+        P_array[i, :, :] = P
     end
 
-    return L
+    return L, P_array
 end
 
 
@@ -51,9 +67,8 @@ end
 Test Riccati equation upon a toy example.
 
 """
-function main(rho = .3)
+function simulate_lqr(Tf=20, rho = .3, sigma=.01)
 
-    Tf = 20
     t = 1:Tf
     y = zeros(Tf)
     ctrl = zeros(Tf)
@@ -67,30 +82,51 @@ function main(rho = .3)
     C = [1 0]
 
     # Define cost matrix corresponding to the following cost:
-    # c(x,u) = x' Q x + u' R u
+    # c(x,u) = x' Q x + u' R uc'e
     R = rho * eye(1)
     Q = C'*C
 
     # Solve Riccati equation:
-    L = solve_riccati_backward(Tf, A, B, Q, R, Q)
+    L, P_arr = solve_riccati_backward(Tf, A, B, Q, R, Q)
     # Instantiate x at time 0:
-    x = [1;0]
+    x = [0;0]
+
+    total_cost = 0
+    theoric_cost = x'* reshape(P_arr[1, :, :], 2, 2) * x
 
     # Simulate system's evolution and apply LQR control:
-    for i=1:Tf
+    for i=1:Tf-1
         # Get gain stored in L:
         K = reshape(L[i, :, :], 1, 2)
         # control is straightforward:
         u = K*x
+        # Simulate perturbation:
+        w = sigma * randn(2)
         # Simulate system's evolution:
-        x = A*x + B*K*x
+        total_cost += (x'*Q*x + u'*R*u)[1]
+        x = A*x + B*K*x + w
         # ... and get observation:
+        theoric_cost += sigma^2 * [1,1]'*reshape(P_arr[i+1, :, :], 2, 2)*[1,1]
         y[i] = (C*x)[1]
         ctrl[i] = u[1]
     end
 
     # Plot result with matplotlib:
-    step(t, y)
-    step(t, ctrl)
+    # step(t, y)
+    # step(t, ctrl)
+    # show()
+    return total_cost, theoric_cost
+end
+
+function benchmark_lqr(n_simu=100::Int64)
+
+    costs = zeros(n_simu)
+
+    for n=1:n_simu
+        costs[n] = simulate_lqr(20, .3, 0.01)[1]
+    end
+
+    println(mean(costs))
+    hist(costs)
     show()
 end
