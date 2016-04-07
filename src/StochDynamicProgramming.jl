b646237d699e419d80cb3e6ba6fb155c3d28d067
Henri-Gerard <hd.gerard@orange.fr>
2016-04-07 15:28:57 +0200

    [FIX] Add extensive formulation in StochDynamicProgramming.jl

--- src/StochDynamicProgramming.jl
+++ src/StochDynamicProgramming.jl
@@ -12,12 +12,13 @@ module StochDynamicProgramming
 
 using JuMP, Distributions
 
-export solve_SDDP, NoiseLaw, simulate_scenarios,
+export solve_SDDP, 
+            NoiseLaw, simulate_scenarios,
         SDDPparameters, LinearDynamicLinearCostSPmodel, set_state_bounds,
-        PiecewiseLinearCostSPmodel,
+        PiecewiseLinearCostSPmodel, extensive_formulation,
         PolyhedralFunction, NextStep, forward_simulations,
         StochDynProgModel, SDPparameters, sdp_optimize,
-        sdp_forward_simulation, sampling
+        sdp_forward_simulation, sampling 
 
 include("objects.jl")
 include("utils.jl")
