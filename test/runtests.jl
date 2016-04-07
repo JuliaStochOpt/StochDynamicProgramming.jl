33c5dd9b3a1172e126cacd365332e4d8704de9de
fpacaud <f.pacaud@efficacity.com>
2016-04-07 14:43:35 +0200

    [UPD] Replace simulation_scenarios call in src/

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,12 +22,9 @@ facts("Probability functions") do
     @fact law.supportSize --> 3
 
     dims = (2, 2, 1)
-    scenarios = simulate_scenarios([law, law], dims)
+    scenarios = simulate_scenarios([law, law], 2)
     @fact typeof(scenarios) --> Array{Float64, 3}
     @fact size(scenarios) --> (2, 2, 1)
-
-    scenarios2 = simulate_scenarios(Normal(), dims)
-    @fact typeof(scenarios2) --> Array{Float64, 3}
 
     # test product of noiselaws:
     support2 = [4, 5, 6]
@@ -109,10 +106,7 @@ facts("SDDP algorithm: 1D case") do
     cost,
     dynamic, laws)
     # Generate scenarios for forward simulations:
-    noise_scenarios = simulate_scenarios(model.noises,
-    (model.stageNumber,
-    params.forwardPassNumber,
-    model.dimNoises))
+    noise_scenarios = simulate_scenarios(model.noises,params.forwardPassNumber)
 
     sddp_costs = 0
     context("Linear cost") do
@@ -250,10 +244,7 @@ facts("SDDP algorithm: 2D case") do
 
 
         # Test a simulation upon given scenarios:
-        noise_scenarios = simulate_scenarios(model.noises,
-        (model.stageNumber,
-        n_simulations,
-        model.dimNoises))
+        noise_scenarios = simulate_scenarios(model.noises,n_simulations)
 
         sddp_costs, stocks = forward_simulations(model, params, V, pbs, noise_scenarios)
 
@@ -445,8 +436,8 @@ facts("SDP algorithm") do
 
             costs_sdp, stocks_sdp, controls_sdp = sdp_forward_simulation(modelSDP,
             paramsSDP,
-            aleas_scen, x0,
-            V_sdp, true )
+            aleas_scen,
+            V_sdp, true)
 
             @fact size(stocks_sdp) --> (3,1,2)
             @fact size(controls_sdp) --> (2,1,2)
