774da6ab315545082fa0a754b8c9db1de089ef9f
fpacaud <francois.pacaud@enpc.fr>
2016-06-17 14:44:42 +0200

    [DEV] Implement parallelization of SDDP in a function

--- examples/parallel_sddp.jl
+++ examples/parallel_sddp.jl
@@ -1,41 +1,57 @@
 
 import StochDynamicProgramming
 
-require("damsvalley.jl")
 
-# Perform n parallel passes:
-N_PARALLEL_COMPUTATIONS = 3
-# Synchronize cuts every n iterations:
-SYNCHRONIZE = 5
+"""
+Solve SDDP in parallel.
 
-# Import model and params in main worker
-# /!\ This line must be before redefinition of seed!
-model, params = init_problem()
+# Arguments
+* `model::SPmodel`:
+    the stochastic problem we want to optimize
+* `param::SDDPparameters`:
+    the parameters of the SDDP algorithm
+* `V::Array{PolyhedralFunction}`:
+    the current estimation of Bellman's functions
+* `n_parallel_pass::Int`: default is 4
+    Number of parallel pass to compute
+* `synchronize::Int`: default is 5
+    Set when to synchronize the cuts between the different processes.
+* `display::Int`: default is 0
+    Says whether to display results or not
 
-# Redefine seeds in every processes to maximize randomness:
-@everywhere srand()
+# Return
+* `V::Array{PolyhedralFunction}`:
+    the collection of approximation of the bellman functions
+"""
+function psolve_sddp(model, params, V; n_parallel_pass=4,
+                     synchronize=5, display=0)
+    # Redefine seeds in every processes to maximize randomness:
+    @everywhere srand()
 
-params.maxItNumber = SYNCHRONIZE
-# First pass of algorithm to define value functions in memory:
-V = StochDynamicProgramming.solve_SDDP(model, params)[1]
+    mitn = params.maxItNumber
+    params.maxItNumber = synchronize
 
-# Count number of available CPU:
-ncpu = nprocs() - 1
-println("\nLaunch simulation on ", ncpu, " processes")
-workers = procs()[2:end]
+    # Count number of available CPU:
+    ncpu = nprocs() - 1
+    (display > 0) && println("\nLaunch simulation on ", ncpu, " processes")
+    workers = procs()[2:end]
 
-# As we distribute computation in n process, we perform forward pass in parallel:
-params.forwardPassNumber = max(1, round(Int, params.forwardPassNumber/ncpu))
+    fpn = params.forwardPassNumber
+    # As we distribute computation in n process, we perform forward pass in parallel:
+    params.forwardPassNumber = max(1, round(Int, params.forwardPassNumber/ncpu))
 
-# Start parallel computation:
-for i in 1:N_PARALLEL_COMPUTATIONS
-    # Distribute computation of SDDP in each process:
-    refs = [@spawnat w StochDynamicProgramming.solve_SDDP(model, params, V, 1)[1] for w in workers]
-    # Catch the result in the main process:
-    V = StochDynamicProgramming.catcutsarray([fetch(r) for r in refs]...)
-    # We clean the resultant cuts:
-    StochDynamicProgramming.remove_redundant_cuts!(V)
-    StochDynamicProgramming.prune_cuts!(model, params, V)
-    println("Lower bound at pass ", i, ": ", StochDynamicProgramming.get_lower_bound(model, params, V))
+    # Start parallel computation:
+    for i in 1:n_parallel_pass
+        # Distribute computation of SDDP in each process:
+        refs = [@spawnat w StochDynamicProgramming.solve_SDDP(model, params, V, display)[1] for w in workers]
+        # Catch the result in the main process:
+        V = StochDynamicProgramming.catcutsarray([fetch(r) for r in refs]...)
+        # We clean the resultant cuts:
+        StochDynamicProgramming.remove_redundant_cuts!(V)
+        (display > 0) && println("Lower bound at pass ", i, ": ", StochDynamicProgramming.get_lower_bound(model, params, V))
+    end
+
+    params.forwardPassNumber = fpn
+    params.maxItNumber = mitn
+    return V
 end
-
