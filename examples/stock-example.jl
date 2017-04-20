46c2f44f2676ab26d8e8f65185abaa04c4e0233f
fpacaud <francoispacaud8@gmail.com>
2017-04-20 16:52:23 +0200

    [FIX] Hotfix broken stock-example

--- examples/stock-example.jl
+++ examples/stock-example.jl
@@ -73,8 +73,8 @@ if run_sddp
     paramSDDP = SDDPparameters(SOLVER,
                                passnumber=10,
                                max_iterations=MAX_ITER)
-    V, pbs = solve_SDDP(spmodel, paramSDDP, 2) # display information every 2 iterations
-    lb_sddp = StochDynamicProgramming.get_lower_bound(spmodel, paramSDDP, V)
+    sddp = solve_SDDP(spmodel, paramSDDP, 2) # display information every 2 iterations
+    lb_sddp = StochDynamicProgramming.get_lower_bound(spmodel, paramSDDP, sddp.bellmanfunctions)
     println("Lower bound obtained by SDDP: "*string(round(lb_sddp,4)))
     toc(); println();
 end
@@ -109,7 +109,7 @@ end
 #srand(1234) # to fix the random seed accross runs
 if run_sddp && run_sdp && test_simulation
     scenarios = StochDynamicProgramming.simulate_scenarios(xi_laws,1000)
-    costsddp, stocks = forward_simulations(spmodel, paramSDDP, pbs, scenarios)
+    costsddp, stocks = forward_simulations(spmodel, paramSDDP, sddp.solverinterface, scenarios)
     costsdp, states, controls = forward_simulations(spmodel,paramSDP, Vs, scenarios)
     println("Simulated relative gain of sddp over sdp: "
             *string(round(200*mean(costsdp-costsddp)/abs(mean(costsddp+costsdp)),3))*"%")
