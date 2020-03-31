8a0b470f9b003c3a4904900506e089a2e8f8d9c8
Sungho Park <mrchaos@naver.com>
2020-04-01 05:05:13 +0900

    Update sddprisk.jl

--- test/sddprisk.jl
+++ test/sddprisk.jl
@@ -30,10 +30,10 @@ using StochDynamicProgramming, JuMP, Clp, CutPruners
     end
 
     # Generate probability laws:
-    laws = Vector{NoiseLaw}(undef,n_stages)
+    laws = Vector{NoiseLaw}([])
     proba = 1/n_aleas*ones(n_aleas)
     for t=1:n_stages
-        laws[t] = NoiseLaw([0, 1, 3, 4, 6], proba)
+        push!(laws, NoiseLaw([0, 1, 3, 4, 6], proba))
     end
 
     # set initial position:
